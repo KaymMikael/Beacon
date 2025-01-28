a147551fd3b021ab74aab54d4949da3232a2ede8
Sero <kmcaltiz@gmail.com>
2025-01-28 11:21:53 +0800

    create a command for dail quote

---
+++ src/commands/utility/dailyQuote.js
@@ -0,0 +1,31 @@
+const { EmbedBuilder, MessageFlags } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  cooldown: 30,
+  data: new SlashCommandBuilder()
+    .setName("daily-quote")
+    .setDescription("Get the quote of the day."),
+  async execute(interaction) {
+    try {
+      const response = await fetch("https://zenquotes.io/api/today");
+      if (!response.ok) {
+        console.log("Not Ok");
+        return;
+      }
+      const result = await response.json();
+      const { q: quote, a: author } = result[0];
+      const quoteEmbed = new EmbedBuilder()
+        .setColor(0x0099ff)
+        .setTitle("Quote of the Day")
+        .setDescription(`${quote} - ${author}`);
+      interaction.channel.send({ embeds: [quoteEmbed] });
+      await interaction.reply({
+        content: "Showed the quote of the day",
+        flags: MessageFlags.Ephemeral,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
