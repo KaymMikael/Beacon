e2628601678e8b6b7cdaa139942e2026b6d9ba25
Sero <kmcaltiz@gmail.com>
2025-01-28 11:22:40 +0800

    quote related command can only be used to #quotes channel, setup a cooldown

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,8 +1,8 @@
-const { Events, MessageFlags } = require("discord.js");
+const { Events, MessageFlags, Collection } = require("discord.js");
 
 const allowedChannelIds = [
   process.env.COMMANDS_CHANNEL_ID,
-  process.env.DAILY_QUOTES_CHANNEL_ID,
+  process.env.QUOTES_CHANNEL_ID,
 ];
 
 // Function to Handle Command Execution
@@ -13,12 +13,51 @@ async function handleCommandInteraction(interaction) {
   if (!allowedChannelIds.includes(interaction.channelId)) {
     await interaction.reply({
       content: "You can only use commands in specific channels.",
+      ephemeral: true,
+    });
+    return;
+  }
+
+  // Ensure the the quote command can only be used in #quotes channel
+  if (
+    (interaction.commandName === "daily-quote" ||
+      interaction.commandName === "quote") &&
+    interaction.channelId !== process.env.QUOTES_CHANNEL_ID
+  ) {
+    await interaction.reply({
+      content: "The quote command can only be used in #quotes channel.",
       flags: MessageFlags.Ephemeral,
     });
     return;
   }
 
   const command = interaction.client.commands.get(interaction.commandName);
+
+  const { cooldowns } = interaction.client;
+
+  if (!cooldowns.has(command.data.name)) {
+    cooldowns.set(command.data.name, new Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.data.name);
+  const defaultCooldownDuration = 3;
+  const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+  if (timestamps.has(interaction.user.id)) {
+    const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const expiredTimestamp = Math.round(expirationTime / 1_000);
+      return interaction.reply({
+        content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`,
+        flags: MessageFlags.Ephemeral,
+      });
+    }
+  }
+
+  timestamps.set(interaction.user.id, now);
+  setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
   if (!command) {
     console.error(`No command matching ${interaction.commandName} was found.`);
@@ -32,12 +71,12 @@ async function handleCommandInteraction(interaction) {
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({
         content: "There was an error while executing this command!",
-        flags: MessageFlags.Ephemeral,
+        ephemeral: true,
       });
     } else {
       await interaction.reply({
         content: "There was an error while executing this command!",
-        flags: MessageFlags.Ephemeral,
+        ephemeral: true,
       });
     }
   }
