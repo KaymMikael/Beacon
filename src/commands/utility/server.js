c07ca8e1db7a356df737737b9198493e4e5ec0d9
Sero <kmcaltiz@gmail.com>
2025-01-28 11:24:40 +0800

    add 30 seconds of cooldown

--- src/commands/utility/server.js
+++ src/commands/utility/server.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
+  cooldown: 30,
   data: new SlashCommandBuilder()
     .setName("server")
     .setDescription("Provides information about the server."),
